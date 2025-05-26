e86dfc6bdc521b717d993da33c9c8471725a94b2
Your Name <you@example.com>
2025-05-26 18:06:50 -0400

    Remote

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+use chrono::Local;
+
 fn main() {
-    println!("Hello, world!");
+    // Get current time
+    let current_time = Local::now();
+    println!("Current time: {}", current_time.format("%Y-%m-%d %H:%M:%S"));
+    
+    // Generate a random number
+    let random_number = rand::thread_rng().gen_range(1..=100);
+    println!("Random number between 1 and 100: {}", random_number);
 }
