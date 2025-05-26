dfdd996b1d94ed147a138bd763abb7ca83745c44
Your Name <you@example.com>
2025-05-26 18:01:19 -0400

    Initial commit of Rust project

---
+++ target/debug/deps/rust_project-919b7e121bafe950.d
@@ -0,0 +1,5 @@
+/home/xingxerx/projects/CosmosCode/CosmosCode/rust_project/target/debug/deps/rust_project-919b7e121bafe950: src/main.rs
+
+/home/xingxerx/projects/CosmosCode/CosmosCode/rust_project/target/debug/deps/rust_project-919b7e121bafe950.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_project.d
@@ -0,0 +1,1 @@
+/home/xingxerx/projects/CosmosCode/CosmosCode/rust_project/target/debug/rust_project: /home/xingxerx/projects/CosmosCode/CosmosCode/rust_project/src/main.rs
