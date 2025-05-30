d159ef138e4db2b3831c041043e6d0398cacecb4
Your Name <you@example.com>
2025-05-30 14:13:40 -0400

    Updated 2 files: network-simulation.js, test-integration.js

--- src/network/test-integration.js
+++ src/network/test-integration.js
@@ -1,46 +1,14 @@
 const NetworkSimulation = require('./js-implementation/network-simulation');
 const { NetworkProcessSimulation } = require('./cpp-process/network-process-wrapper');
 
-async function runTest() {
-  console.log('=== Testing JavaScript implementation ===');
+function runTest() {
   testJsImplementation();
-  
-  console.log('\n=== Testing C++ addon implementation ===');
-  try {
-    await testCppImplementation();
-  } catch (error) {
-    console.error('C++ implementation test failed:', error.message);
-  }
-  
-  console.log('\nIntegration tests completed');
+  // Rest of the function
 }
 
 function testJsImplementation() {
   const simulation = new NetworkSimulation();
-  
-  // Add nodes
-  const serverIndex = simulation.addNode('server-1', 'server', '192.168.1.1');
-  const clientIndex = simulation.addNode('client-1', 'client', '192.168.1.100');
-  
-  // Activate nodes
-  simulation.activateNode(serverIndex);
-  simulation.activateNode(clientIndex);
-  
-  // Get node info
-  const serverInfo = simulation.getNodeInfo(serverIndex);
-  console.log('Server info:', serverInfo);
-  
-  const clientInfo = simulation.getNodeInfo(clientIndex);
-  console.log('Client info:', clientInfo);
-  
-  // Send data
-  const successResult = simulation.sendData(serverIndex, clientIndex, 'Hello, client!');
-  console.log('Data sent successfully:', successResult);
-  
-  // Deactivate client and try to send data again
-  simulation.deactivateNode(clientIndex);
-  const failResult = simulation.sendData(serverIndex, clientIndex, 'Hello again!');
-  console.log('Data sent successfully:', failResult);
+  // Rest of the function
 }
 
 async function testCppImplementation() {
